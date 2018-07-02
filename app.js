696d6f60fd81fc66d0fcd3091b6e027cf300fd21
Sara Bracewell <bracewell.sara@gmail.com>
2018-07-02 09:20:37 -0500

    updated files with ejs

--- app.js
+++ app.js
@@ -1,18 +1,35 @@
-var express = require("express"),
+const express = require("express"),
     app = express();
+//view engine which will handle the front end side, which is ejs
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
 
+// const user = {
+//     firstname: 'Sara',
+//     lastname: 'Bracewell'
+// }
+
+// let data = "<h1>Testing</h1>"
+
+// Below are HTTP methods. Routes for webpages, getting the static files
+// route home page to render the home.ejs file
 app.get("/", function (req, res) {
-    res.send("<h1>Home Page</h1>");
+    res.render('home');
 });
 
+//route for about page
 app.get("/about", function (req, res) {
-    res.send("<h1>About Page</h1>");
+    res.render('about');
 });
 
+app.get("/contact", function (req, res) {
+    res.render('contact');
+});
 
-var server = app.listen(3000, function(err){
+// Start the server and give error if it is not working
+var server = app.listen(3000, function(err) {
     if(err) {
-        console.log(err);
+        console.log('There is a problem with the server: ' + err);
     }
     console.log('App started: Port 3000');
 });
