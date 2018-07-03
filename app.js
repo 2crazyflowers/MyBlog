15199c883548b8193238db0c52886f0afda38540
Sara Bracewell <bracewell.sara@gmail.com>
2018-07-02 20:48:39 -0500

    updated ejs on blog page with forEach loop from var in app.js

--- app.js
+++ app.js
@@ -1,5 +1,31 @@
 const express = require("express"),
     app = express();
+
+    // var data = {
+    //     title: "Hello Everyone"
+    // }
+    var posts = [
+        {
+            postTitle: "Test Title 1",
+            postSubTitle: "This is a subtitle",
+            postLink: "https://www.linkedin.com/in/sara-bracewell-3b952761/",
+            postDate: "July 2, 2018"
+        },
+        {
+            postTitle: "Test Title 2",
+            postSubTitle: "This is a subtitle",
+            postLink: "https://www.linkedin.com/in/sara-bracewell-3b952761/",
+            postDate: "July 14, 2018"
+        },
+        {
+            postTitle: "Test Title 3",
+            postSubTitle: "This is a subtitle",
+            postLink: "https://www.linkedin.com/in/sara-bracewell-3b952761/",
+            postDate: "July 2, 2018"
+        }
+    ]
+
+
 //view engine which will handle the front end side, which is ejs
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
@@ -14,7 +40,7 @@ app.use(express.static('public'));
 // Below are HTTP methods. Routes for webpages, getting the static files
 // route home page to render the home.ejs file
 app.get("/", function (req, res) {
-    res.render('home');
+    res.render('home', {posts:posts});
 });
 
 //route for about page
