ff747d7de50648e46bc305fdf95266b806e0ab5b
Sara Bracewell <bracewell.sara@gmail.com>
2018-07-09 14:26:10 -0500

    mongo is now connected and user is saved to mongodb

--- app.js
+++ app.js
@@ -31,8 +31,17 @@ var express         = require("express"),
 
 
 //App Configuration
-// mongoose.Promise = Promise;
-mongoose.connect("mongodb://localhost/MyBlogApp"); //if the database is not there it will create it
+mongoose.Promise = Promise;
+
+mongoose.connect("mongodb://localhost:27017/MyBlogApp")
+    .then(() => {
+        console.log('The database connected')
+    })
+    .catch((err)=> {
+        console.log('Something went awry', err);
+    });
+
+//if the database is not there it will create it
 // View engine which will handle the front end side, which is ejs
 app.set('view engine', 'ejs'); 
 // Use express.static to serve the public folder as a static directory
@@ -49,11 +58,51 @@ app.use(require("express-session")({
     saveUninitalized: false
 }));
 
+// catch 404 and forward to error handler
+// app.use(function(req, res, next) {
+//     var err = new Error('Not Found');
+//     err.status = 404;
+//     next(err);
+// });
+
+// // error handler
+// app.use(function(err, req, res, next) {
+//     // set locals, only providing error in development
+//     res.locals.message = err.message;
+//     res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+//     // render the error page
+//     res.status(err.status || 500);
+//     res.render('error');
+// });
+
 app.use(passport.initialize());
 app.use(passport.session());
 passport.use(new LocalStrategy(User.authenticate()));
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+// passport.serializeUser(User.serializeUser());
+// passport.deserializeUser(User.deserializeUser());
+//from documentation
+passport.serializeUser(function(user, done) {
+    done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+    User.findById(id, function (err, user) {
+        done(err, user);
+    });
+});
+
+// passport.use(new LocalStrategy(
+//     function(username, password, done) {
+//       User.findOne({ username: username }, function (err, user) {
+//         if (err) { return done(err); }
+//         if (!user) { return done(null, false); }
+//         if (!user.verifyPassword(password)) { return done(null, false); }
+//         return done(null, user);
+//       });
+//     }
+//   ));
+
 
 // Below are HTTP methods. Routes for webpages, getting the static files
 // route home page to render the home.ejs file
@@ -84,10 +133,26 @@ app.get("/signup", function (req, res) {
 
 app.post("/signup", function (req, res) {
     //console.log(req.body) - add name to input in form for user/password
-    console.log(req.body.username);
-    console.log(req.body.password);
+    console.log('Current username: ' + req.body.username);
+    console.log('Current password: ' + req.body.password);
 
-    var newUser = new User({username:req.body.username});
+    // Create a new user using req.body
+    // User.create(req.body)
+    //     .then(function(dbUser) {
+    //     // If saved successfully, send the the new User document to the client
+    //     res.json(dbUser);
+    //     })
+    //     .catch(function(err) {
+    //     // If an error occurs, send the error to the client
+    //     res.json(err);
+    //     });
+
+    var newUser = new User({
+        username: req.body.username,
+        password: req.body.password
+    });
+    console.log(newUser);
+
     User.register(newUser, req.body.password, function(err, newCreatedUser){
         if(err) {
             console.log('There is an error signing up: ' + err);
@@ -95,8 +160,9 @@ app.post("/signup", function (req, res) {
         }
         passport.authenticate('local')(req, res, function(){
             res.redirect('/');
-        })
-    })
+        });
+    });
+    
 });
 
 //route for add new blog page
--- models/userModel.js
+++ models/userModel.js
@@ -1,11 +1,16 @@
-var mongoose = require('mongoose'),
-    passportLocalMongoose = require('passport-local-mongoose');
+var mongoose = require('mongoose');
+var passportLocalMongoose = require('passport-local-mongoose');
 
 var UserSchema = new mongoose.Schema({
-    username: {type: String, required: "Cannot be empty"},
-    password: {type: String, required: "Cannot be empty"}
+    // username: {type: String, required: "Cannot be empty"},
+    // password: {type: String, required: "Cannot be empty"}
+    
+    username: {type: String},
+    password: {type: String},
+    // _id:      { type: mongoose.Schema.Types.ObjectId },
 });
 
 UserSchema.plugin(passportLocalMongoose);
 
+// Export the User model
 module.exports = mongoose.model("User", UserSchema); 
